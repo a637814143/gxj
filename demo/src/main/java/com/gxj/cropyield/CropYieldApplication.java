ebd912838d44d2d3f0dcf4fa422947f8804d73c5
a637814143 <140132912+a637814143@users.noreply.github.com>
2025-10-15 15:23:48 +0800

    feat: implement jwt login

--- demo/src/main/java/com/gxj/cropyield/CropYieldApplication.java
+++ demo/src/main/java/com/gxj/cropyield/CropYieldApplication.java
@@ -2,8 +2,16 @@ package com.gxj.cropyield;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "com.gxj.cropyield.modules",
+    "com.gxj.cropyield.common",
+    "com.gxj.cropyield.config"
+})
+@EntityScan(basePackages = "com.gxj.cropyield.modules")
+@EnableJpaRepositories(basePackages = "com.gxj.cropyield.modules")
 public class CropYieldApplication {
 
     public static void main(String[] args) {
--- demo/src/main/java/com/gxj/cropyield/crop/CropController.java
+++ demo/src/main/java/com/gxj/cropyield/crop/CropController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController("legacyCropController")
 @RequestMapping("/api/crops")
 public class CropController {
 
